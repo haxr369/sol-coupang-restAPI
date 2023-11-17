069f527751b5379cc29afe967dcbf8f88a5092f8
ohchansol <haxr369@gmail.com>
2023-11-17 09:40:03 +0900

    fix start sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Spring Boot 애플리케이션을 백그라운드로 실행
-nohup java -Dserver.port=80 -jar /var/www/jar/app.jar > /var/www/logs/app.log 2>&1 &
+nohup java -Dserver.port=80 -jar /var/www/jar/app.jar  &
 
 # 백그라운드 실행 확인 메시지
 echo "Spring Boot 애플리케이션이 백그라운드에서 실행 중입니다."
