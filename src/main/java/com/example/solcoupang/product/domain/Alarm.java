00c84193aa046e4d157b3539560fec05e5398433
ohchansol <haxr369@gmail.com>
2023-11-19 19:11:24 +0900

    create alarm entity

---
+++ src/main/java/com/example/solcoupang/product/domain/Alarm.java
@@ -0,0 +1,24 @@
+package com.example.solcoupang.product.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Alarm {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long alarmId;
+    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "user_id")
+//    private User user;
+    private String message;
+    @Column(name = "read_or_not")
+    private boolean readOrNot;
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+}
